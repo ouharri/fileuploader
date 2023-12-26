705c2aac98016d546367eb2942123de8ae8e55d8
ouharri <ouharrioutman@gmail.com>
2023-12-26 20:29:52 +0100

    Service class for handling file-related operations.

--- src/main/java/com/ouharri/fileuploader/service/FileStorageService.java
+++ src/main/java/com/ouharri/fileuploader/service/FileStorageService.java
@@ -1,2 +1,70 @@
-package com.ouharri.fileuploader.service;public class FileStorageService {
+package com.ouharri.fileuploader.service;
+
+import com.ouharri.fileuploader.entity.FileDB;
+import com.ouharri.fileuploader.exception.ResourceNotCreatedException;
+import com.ouharri.fileuploader.exception.ResourceNotFoundException;
+import com.ouharri.fileuploader.repository.FileDBRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.Objects;
+import java.util.UUID;
+import java.util.stream.Stream;
+
+/**
+ * Service class for handling file-related operations.
+ */
+@Service
+@AllArgsConstructor
+public class FileStorageService {
+
+    private final FileDBRepository fileDBRepository;
+
+    /**
+     * Store a file in the database.
+     *
+     * @param file The file to be stored.
+     * @return The stored FileDB entity.
+     * @throws IOException If an I/O exception occurs while reading the file data.
+     */
+    public FileDB store(MultipartFile file) throws IOException {
+        String fileName = StringUtils.cleanPath(Objects.requireNonNull(file.getOriginalFilename()));
+        FileDB fileDB = FileDB.builder()
+                .name(fileName)
+                .type(file.getContentType())
+                .data(file.getBytes())
+                .build();
+
+        try {
+            return fileDBRepository.save(fileDB);
+        } catch (ResourceNotCreatedException e) {
+            throw new ResourceNotCreatedException("Could not store file " + fileName + ". Please try again!");
+        }
+    }
+
+    /**
+     * Get a file from the database by its ID.
+     *
+     * @param id The unique identifier of the file.
+     * @return The FileDB entity.
+     * @throws ResourceNotFoundException If the file with the specified ID is not found.
+     */
+    public FileDB getFile(UUID id) {
+        return fileDBRepository.findById(id)
+                .orElseThrow(() ->
+                        new ResourceNotFoundException("File not found with id " + id)
+                );
+    }
+
+    /**
+     * Get a stream of all files in the database.
+     *
+     * @return A stream of FileDB entities.
+     */
+    public Stream<FileDB> getAllFiles() {
+        return fileDBRepository.findAll().stream();
+    }
 }
