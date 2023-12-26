bc09cf7d8ff16f2616c9da0102dd2fe7ec55fd3d
ouharri <ouharrioutman@gmail.com>
2023-12-26 20:07:49 +0100

    Configuration class for Spring Security settings.

--- src/main/java/com/ouharri/fileuploader/config/SecurityConfiguration.java
+++ src/main/java/com/ouharri/fileuploader/config/SecurityConfiguration.java
@@ -1,17 +1,13 @@
-package com.ouharri.aftas.security;
+package com.ouharri.fileuploader.config;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.security.web.authentication.logout.LogoutHandler;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
@@ -30,15 +26,8 @@ import static org.springframework.security.config.http.SessionCreationPolicy.STA
 @EnableMethodSecurity
 public class SecurityConfiguration {
     private static final String[] WHITE_LIST_URL = {
-            "/api/v2/**",
-            "/api/v2/auth/**",
-            "/v2/api-docs",
-            "/v3/api-docs",
-            "/v3/api-docs/**",
-            "/swagger-resources",
-            "/swagger-resources/**",
-            "/configuration/ui",
-            "/configuration/security",
+            "/files/**",
+            "/upload/**",
             "/swagger-ui/**",
             "/webjars/**",
             "/swagger-ui.html"
@@ -50,8 +39,6 @@ public class SecurityConfiguration {
             "GET",
             "POST",
             "PUT",
-            "DELETE",
-            "OPTIONS"
     };
     private static final String[] ALLOW_HEAD = {
             "Access-Control-Allow-Origin",
@@ -66,9 +53,6 @@ public class SecurityConfiguration {
             "Authorization",
             "X-Requested-With"
     };
-    private final JwtAuthenticationFilter jwtAuthFilter;
-    private final AuthenticationProvider authenticationProvider;
-    private final LogoutHandler logoutHandler;
 
     /**
      * Configures security settings for the application, including authentication, authorization, and JWT handling.
@@ -95,15 +79,6 @@ public class SecurityConfiguration {
                 )
                 .sessionManagement(session ->
                         session.sessionCreationPolicy(STATELESS)
-                )
-                .authenticationProvider(authenticationProvider)
-                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
-                .logout(logout ->
-                        logout.logoutUrl("/api/v1/auth/logout")
-                                .addLogoutHandler(logoutHandler)
-                                .logoutSuccessHandler((request, response, authentication) ->
-                                        SecurityContextHolder.clearContext()
-                                )
                 );
         return http.build();
     }
