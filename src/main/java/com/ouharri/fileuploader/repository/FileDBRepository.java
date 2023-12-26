c11c9e6ed686271a5bd3b155c5e52d9f79ce9727
ouharri <ouharrioutman@gmail.com>
2023-12-26 16:44:51 +0100

    Repository interface for performing CRUD operations on FileDB entities.

--- src/main/java/com/ouharri/fileuploader/repository/FileDBRepository.java
+++ src/main/java/com/ouharri/fileuploader/repository/FileDBRepository.java
@@ -1,2 +1,15 @@
-package com.ouharri.fileuploader.repository;public class FileDBRepository {
+package com.ouharri.fileuploader.repository;
+
+import com.ouharri.fileuploader.entity.FileDB;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+/**
+ * Repository interface for performing CRUD operations on FileDB entities.
+ */
+@Repository
+public interface FileDBRepository extends JpaRepository<FileDB, UUID> {
+
 }
