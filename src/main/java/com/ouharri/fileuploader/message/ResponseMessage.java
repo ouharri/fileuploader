6cf6aaa6544517a3145d1ee1b3143a063c5bc6e8
ouharri <ouharrioutman@gmail.com>
2023-12-26 20:38:46 +0100

    Response object representing a simple message for client communication.

--- src/main/java/com/ouharri/fileuploader/message/ResponseMessage.java
+++ src/main/java/com/ouharri/fileuploader/message/ResponseMessage.java
@@ -1,2 +1,19 @@
-package com.ouharri.fileuploader.message;public class ResponseMessage {
+package com.ouharri.fileuploader.message;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * Response object representing a simple message for client communication.
+ */
+@Getter
+@Setter
+@AllArgsConstructor
+public class ResponseMessage {
+
+    /**
+     * The message content.
+     */
+    private String message;
 }
