fc3740890b45baf07f6ffd77b88757aa4a000f77
ouharri <ouharrioutman@gmail.com>
2023-12-27 12:11:24 +0100

    Configuration class for Redis And app caching.

--- src/main/java/com/ouharri/fileuploader/config/RedisConfig.java
+++ src/main/java/com/ouharri/fileuploader/config/RedisConfig.java
@@ -1,6 +1,7 @@
 package com.ouharri.fileuploader.config;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
@@ -13,9 +14,10 @@ import org.springframework.data.redis.serializer.RedisSerializationContext;
 import java.time.Duration;
 
 /**
- * Configuration class for Redis.
+ * Configuration class for Redis And app caching.
  */
 @Configuration
+@EnableCaching
 public class RedisConfig {
 
     @Value("${redis.host}")
