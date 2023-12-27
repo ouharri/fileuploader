f908d557260495b3fff3627ac5648018c3c6bcd1
ouharri <ouharrioutman@gmail.com>
2023-12-27 02:11:50 +0100

    Integration tests for the FileUploader application. These tests ensure that the application context loads successfully.

---
+++ src/test/java/com/ouharri/fileuploader/FileUploaderTests.java
@@ -0,0 +1,21 @@
+package com.ouharri.fileuploader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * Integration tests for the FileUploader application.
+ * These tests ensure that the application context loads successfully.
+ */
+@SpringBootTest
+class FileUploaderTests {
+
+    /**
+     * Test to verify that the application context loads without errors.
+     */
+    @Test
+    void contextLoads() {
+        // The test is empty as it only checks if the context loads successfully.
+    }
+
+}
