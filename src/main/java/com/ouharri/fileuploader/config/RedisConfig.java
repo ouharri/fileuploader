298d84f44f1042042d39d47564898e920167d8c7
ouharri <ouharrioutman@gmail.com>
2024-01-28 18:02:12 +0000

    Configuration class for Redis And app caching.

--- src/main/java/com/ouharri/fileuploader/config/RedisConfig.java
+++ src/main/java/com/ouharri/fileuploader/config/RedisConfig.java
@@ -20,10 +20,10 @@ import java.time.Duration;
 @EnableCaching
 public class RedisConfig {
 
-    @Value("${redis.host}")
+    @Value("${spring.data.redis.host}")
     private String redisHost;
 
-    @Value("${redis.port}")
+    @Value("${spring.data.redis.port}")
     private int redisPort;
 
     /**
